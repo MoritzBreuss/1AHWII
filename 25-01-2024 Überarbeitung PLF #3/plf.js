09475f28fc4106cfdacd7d53769255f23ba18b36
Moritz Breuss <BRE230090@spengergasse.at>
2024-02-01 13:37:59 +0100

    vereinfacht

--- 25-01-2024 Überarbeitung PLF #3/plf.js
+++ 25-01-2024 Überarbeitung PLF #3/plf.js
@@ -15,13 +15,10 @@ class Person {
   }
 
   vorName() {
-    const names = this.name.split(" ");
-    return names[0];
+    return this.name.split(" ", 1)[0];
   }
-
   nachName() {
-    const names = this.name.split(" ");
-    return names.slice(1).join(" ");
+    return this.name.split(" ")[1];
   }
 
   toString() {
@@ -29,8 +26,7 @@ class Person {
   }
 
   getBmi() {
-    const bmi = this.gewichtKG / this.groesseM ** 2;
-    return parseFloat(bmi.toFixed(2));
+    return this.gewichtKG / Math.pow(this.groesseM, 2);
   }
 
   getGewichtType() {
