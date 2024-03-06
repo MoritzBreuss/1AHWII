8458b069902f1d5085857213869b943ca763dcb4
Moritz Breuss <BRE230090@spengergasse.at>
2024-03-06 22:19:37 +0100

    fakerDE

--- 2024-02-29__node_faker/script.js
+++ 2024-02-29__node_faker/script.js
@@ -3,3 +3,23 @@ const { fakerDE } = require('@faker-js/faker');
 for (let i = 0; i < 10; i++) {
     console.log(fakerDE.finance.iban());
 }
+
+for (let i = 0; i < 10; i++) {
+    console.log(fakerDE.animal.fish());
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(fakerDE.color.rgb());
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(fakerDE.person.fullName());
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(fakerDE.number.hex());
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(fakerDE.vehicle.bicycle());
+}
